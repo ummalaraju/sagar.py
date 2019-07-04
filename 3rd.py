20de212969d55efb793bcb8031bbdd580c8a1622
ummalaraju <52439451+ummalaraju@users.noreply.github.com>
2019-07-04 10:58:39 +0530

    Add files via upload

---
+++ 3rd.py
@@ -0,0 +1,8 @@
+num =int(input("enter any number: "))
+flag = num%2
+if flag == 0:
+print(number,"is even number")
+elif flag == 1:
+print(number,"is odd number")
+else:
+print("error,invalid input")
